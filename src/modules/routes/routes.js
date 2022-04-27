7eae57c11a7588fe40f7b2e2b3fb6ed849067d37
ChinaevVladimir <chinaev.exceedteam@gmail.com>
2022-04-27 14:54:27 +0300

    fix code style

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const { getAllTasks, createNewTask } = require("../controllers/taskController");
 
 router.get("/allTasks", getAllTasks);
